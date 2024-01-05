2da08a78f6fbb7f85ca9e155e7cb5386c527082e
jaypan <pan@peaq.io>
2024-01-05 14:21:56 +0100

    Refine the all configuration file

---
+++ scripts/forked-peaq.sh
@@ -0,0 +1,6 @@
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.1.yml" \
+RPC_ENDPOINT="https://erpc-mpfn1.peaq.network" \
+DOCKER_COMPOSE_FOLDER="yoyo" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.1" \
+KEEP_COLLATOR="false" \
+sh -e -x forked.generated.sh

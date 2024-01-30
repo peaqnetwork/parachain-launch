7fb6ab9346a4067382393ff4624c8ba9497ddfc0
jaypan <pan@peaq.io>
2024-01-30 12:51:38 +0100

    Update version

--- scripts/forked-moonbeam.sh
+++ scripts/forked-moonbeam.sh
@@ -1,7 +1,7 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.moonbeam.v0.0.11.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.moonbeam.v0.0.13.yml" \
 RPC_ENDPOINT="https://moonbeam.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-moonbeam-v0.0.11" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-moonbeam-v0.0.13" \
 KEEP_COLLATOR="false" \
 KEEP_PARACHAIN="false" \
 sh -e -x forked.generated.sh

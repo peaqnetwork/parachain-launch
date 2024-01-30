01efc918688d97dcf303cf86e263f10b6170ec5b
jaypan <pan@peaq.io>
2024-01-30 10:52:34 +0100

    Update the scripts

--- scripts/forked-krest.sh
+++ scripts/forked-krest.sh
@@ -1,6 +1,7 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.krest.forked.v0.0.4.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.krest.forked.v0.0.5.yml" \
 RPC_ENDPOINT="https://erpc-krest.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/krest-v0.0.4" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/krest-v0.0.5" \
 KEEP_COLLATOR="false" \
+KEEP_PARACHAIN="true" \
 sh -e -x forked.generated.sh
---
+++ scripts/forked-moonbeam.sh
@@ -0,0 +1,7 @@
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.moonbeam.v0.0.11.yml" \
+RPC_ENDPOINT="https://moonbeam.peaq.network" \
+DOCKER_COMPOSE_FOLDER="yoyo" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-moonbeam-v0.0.11" \
+KEEP_COLLATOR="false" \
+KEEP_PARACHAIN="false" \
+sh -e -x forked.generated.sh

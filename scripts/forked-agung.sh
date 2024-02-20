c702541bd861e984b972a65be787d1d12c5a1e6d
jaypan <pan@peaq.io>
2024-02-20 15:57:30 +0100

    Add the argumens + update the peaq-dev config

--- scripts/forked-agung.sh
+++ scripts/forked-agung.sh
@@ -1,7 +1,8 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.10.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.11.yml" \
 RPC_ENDPOINT="https://rpcpc1-qa.agung.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.10" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.11" \
 KEEP_COLLATOR="false" \
+KEEP_ASSET="true" \
 KEEP_PARACHAIN="true" \
 sh -e -x forked.generated.sh

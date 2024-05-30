8b8a013bc9151a6f6dac95b9ad6f7e44025774b2
jaypan <pan@peaq.io>
2024-05-30 14:28:26 +0200

    Update more information and add one script

--- scripts/run-parachain-collator.sh
+++ scripts/run-parachain-collator.sh
@@ -6,11 +6,27 @@
 KEEP_CHAIN=${KEEP_CHAIN:-"false"}
 SURI=${SURI}
 
-PARACHAIN_ID="2000"
-PARACHAIN_CONFIG="/home/jaypan/Work/peaq/parachain-launch/yoyo/dev-local-"${PARACHAIN_ID}".json"
-PARACHAIN_BOOTNODE="/ip4/127.0.0.1/tcp/40336/p2p/12D3KooWPB4WuY3hxh42rwDcxZcezQYTiozVKkypwX1qrjQ38fDc"
+TYPE="peaq"
+VERSION="v0.0.6"
+PARACHAIN_BOOTNODE="/ip4/127.0.0.1/tcp/40336/p2p/12D3KooWRGKzxojMtqPFdRT9d3oDKtJCfa2RHTS8sAp5uvE4SM7E"
+
+if [ "$TYPE" == "peaq-dev" ]; then
+  PARACHAIN_ID="2000"
+  CONFIG_NAME="dev-local-${PARACHAIN_ID}.json"
+elif [ "$TYPE" == "krest" ]; then
+  PARACHAIN_ID="2241"
+  CONFIG_NAME="krest-local-${PARACHAIN_ID}.json"
+elif [ "$TYPE" == "peaq" ]; then
+  PARACHAIN_ID="3338"
+  CONFIG_NAME="peaq-local-${PARACHAIN_ID}.json"
+else
+  echo "Unknown parachain type: $TYPE"
+  exit 1
+fi
+
+PARACHAIN_CONFIG="/home/jaypan/Work/peaq/parachain-launch/yoyo/$CONFIG_NAME"
 RELAYCHAIN_CONFIG="/home/jaypan/Work/peaq/parachain-launch/yoyo/rococo-local.json"
-BINARY_PATH="/home/jaypan/PublicSMB/peaq-node-binary/peaq-node.peaq-dev.v0.0.16"
+BINARY_PATH="/home/jaypan/PublicSMB/peaq-node-binary/peaq-node.${TYPE}.${VERSION}"
 CHAIN_FOLDER="chain-folder-need-delete"
 
 # main sub-libp2p: [Parachain] 🏷n  Local node identity is: 12D3KooWSKbKXoRM1Au6ycCQAAS6ESCMNLBkbEC84Zd9ysQXJsrD

3a5589cfcf31622759b3cd5687df9f90f777e439
jaypan <pan@peaq.io>
2024-06-18 15:34:33 +0200

    Update information/ci.config

--- scripts/forked-agung.sh
+++ scripts/forked-agung.sh
@@ -1,7 +1,7 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.16.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.17.yml" \
 RPC_ENDPOINT="https://rpcpc1-qa.agung.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.16" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.17" \
 KEEP_COLLATOR="false" \
 KEEP_ASSET="true" \
 KEEP_PARACHAIN="false" \

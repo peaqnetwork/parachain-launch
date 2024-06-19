e37b6c1ba182d3ecf17b9a7f28a65f7633281299
jaypan <pan@peaq.io>
2024-06-19 13:40:51 +0200

    Update

--- scripts/forked-krest.sh
+++ scripts/forked-krest.sh
@@ -3,6 +3,6 @@ RPC_ENDPOINT="https://erpc-krest.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
 FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/krest-v0.0.7" \
 KEEP_COLLATOR="false" \
-KEEP_ASSETS="false" \
+KEEP_ASSET="false" \
 KEEP_PARACHAIN="false" \
 sh -e -x forked.generated.sh

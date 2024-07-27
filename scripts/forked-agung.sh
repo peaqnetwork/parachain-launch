e1ea1758df052b594c6ab61bb2943df7e76fc348
jaypan <pan@peaq.io>
2024-07-27 21:16:05 +0200

    Refinie the scripts

--- scripts/forked-agung.sh
+++ scripts/forked-agung.sh
@@ -1,7 +1,7 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.17.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.18.yml" \
 RPC_ENDPOINT="https://rpcpc1-qa.agung.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.17" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.18" \
 KEEP_COLLATOR="false" \
 KEEP_ASSET="true" \
 KEEP_PARACHAIN="false" \

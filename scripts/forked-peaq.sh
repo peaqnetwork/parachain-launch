77b8a65730518305569245e84aa6bc844d4847a4
jaypan <pan@peaq.io>
2024-08-06 15:37:43 +0200

    Update scripts to 10min session due to reducing the reorg

--- scripts/forked-peaq.sh
+++ scripts/forked-peaq.sh
@@ -1,7 +1,7 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.6.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.7.yml" \
 RPC_ENDPOINT="https://erpc-mpfn1.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.6" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.7" \
 KEEP_COLLATOR="false" \
 KEEP_ASSET="false" \
 KEEP_PARACHAIN="false" \

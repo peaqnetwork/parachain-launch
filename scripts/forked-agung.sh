85a928e88669b36290320d98b0abbe161110dfe1
jaypan <pan@eotlabs.io>
2025-01-20 14:23:03 +0000

    Update the scripts

--- scripts/forked-agung.sh
+++ scripts/forked-agung.sh
@@ -1,8 +1,8 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.102.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.103.yml" \
 RPC_ENDPOINT="https://wss-async.agung.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
 RELAY_CHAIN_CONFIG_FILE="rococo-local.json" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.102" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.103" \
 KEEP_COLLATOR="false" \
 KEEP_ASSET="true" \
 KEEP_PARACHAIN="false" \
--- scripts/forked-peaq.sh
+++ scripts/forked-peaq.sh
@@ -1,8 +1,8 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.103.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.104.yml" \
 RPC_ENDPOINT="https://peaq.api.onfinality.io/public" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
 RELAY_CHAIN_CONFIG_FILE="rococo-local.json" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.103" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.104" \
 KEEP_COLLATOR="false" \
 KEEP_ASSET="true" \
 KEEP_PARACHAIN="false" \

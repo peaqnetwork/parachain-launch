c390900d56138c3163a8470733db2e4786435599
jaypan <pan@eotlabs.io>
2025-02-05 12:28:25 +0000

    Update the config + scripts

--- scripts/forked-agung.sh
+++ scripts/forked-agung.sh
@@ -1,5 +1,5 @@
 FORKED_CONFIG_FILE="scripts/config.parachain.peaq-dev.forked.v0.0.103.yml" \
-RPC_ENDPOINT="https://wss-async.agung.peaq.network" \
+RPC_ENDPOINT="wss://wss-async.agung.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
 RELAY_CHAIN_CONFIG_FILE="rococo-local.json" \
 FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-dev-v0.0.103" \

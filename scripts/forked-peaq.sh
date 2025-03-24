45dd968c2bad369813a671ba531e70037c254279
jaypan <pan@eotlabs.io>
2025-03-24 17:58:42 +0100

    Update the verion + fork-off project

--- scripts/forked-peaq.sh
+++ scripts/forked-peaq.sh
@@ -1,8 +1,8 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.104.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.105.yml" \
 RPC_ENDPOINT="wss://mpfn1.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
 RELAY_CHAIN_CONFIG_FILE="rococo-local.json" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.104" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.105" \
 KEEP_COLLATOR="false" \
 KEEP_ASSET="true" \
 KEEP_PARACHAIN="false" \

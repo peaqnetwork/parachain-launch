bbd451d174f1680545b9d2d31f48a3249678bbf0
jaypan <pan@eotlabs.io>
2025-09-24 11:11:16 +0200

    Update Peaq parachain configs from v0.0.109 to v0.0.110

--- scripts/forked-peaq.sh
+++ scripts/forked-peaq.sh
@@ -1,11 +1,12 @@
-FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.109.yml" \
+FORKED_CONFIG_FILE="scripts/config.parachain.peaq.forked.v0.0.110.yml" \
 RPC_ENDPOINT="wss://mpfn1.peaq.network" \
 DOCKER_COMPOSE_FOLDER="yoyo" \
 RELAY_CHAIN_CONFIG_FILE="rococo-local.json" \
-FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.109" \
+FORK_FOLDER="/home/jaypan/Work/peaq/fork-test/fork-binary/peaq-v0.0.110" \
 KEEP_COLLATOR="false" \
 KEEP_ASSET="true" \
 KEEP_PARACHAIN="false" \
 sh -e -x forked.generated.sh
 
+# NO_IGNORE_SIZE=1000 \
 # In the docker enviroment, we should use westend-local.json to reduce the session length
